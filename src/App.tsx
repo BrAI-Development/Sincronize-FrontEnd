ccd308feb9495268a45bab7093bb534a7df76a99
miusarname2 <oscarmauricio631@gmail.com>
2025-02-08 22:09:26 -0500

    feat: :sparkles: The app is uploaded

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { AdminUsers } from './pages/AdminUsers';
+import { Login } from './pages/Login';
+import { TimerPage } from './pages/TimerPage';
+import { UserAdministrate } from './pages/UserAdministrate';
+
+function App() {
+  return <AdminUsers />;
+}
+
+export default App;
